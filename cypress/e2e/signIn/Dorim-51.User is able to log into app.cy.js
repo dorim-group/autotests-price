7fb374f65d61c6ba2cf2040fedbc4f035d00c724
Anton Tkachuk <anton.tkachuk@namecheap.com>
2024-10-04 10:24:35 +0300

    test commit to pr

--- cypress/e2e/signIn/Dorim-51.User is able to log into app.cy.js
+++ cypress/e2e/signIn/Dorim-51.User is able to log into app.cy.js
@@ -6,7 +6,7 @@ describe("Dorim-51.User is able to log into app", () => {
   it("Dorim-51.User is able to log into app", () => {
     cy.fixture("LoginPrice").then((data) => {
         cy.priceLogin(data);
-        cy.priceLogout()
+        cy.priceLogout();
     })
   })
 });
