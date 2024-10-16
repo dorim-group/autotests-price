53ad3a28a6dd2eff81da5017f5106eabaed3ec73
Maxim Voronin <maxim.voronin2@gmail.com>
2024-10-16 15:02:48 +0500

    fixed syntax, formatting by prettier

--- cypress/e2e/product-selection/PRICE-57.User moves into checkout.cy.js
+++ cypress/e2e/product-selection/PRICE-57.User moves into checkout.cy.js
@@ -3,11 +3,11 @@ import checkoutPage from "../../pages/checkout";
 import { textContent, urls } from "../../valid-data/info/validInfo";
 
 describe("PRICE-57.User moves to checkout page", () => {
-    it("PRICE-57.User moves to checkout page", { tags: ["dev","stage"] }, () => {
-        cy.setAuthToken();
-        const page = new checkoutPage
-        page.addDrugToTheCart(0)
-        page.checkDrugStatus()
-        cy.url().should("include", urls.productSelectioCheckoutPage)
-    });
+  it("PRICE-57.User moves to checkout page", { tags: ["dev", "stage"] }, () => {
+    cy.setAuthToken();
+    const page = new checkoutPage();
+    page.addDrugToTheCart(0);
+    page.checkDrugStatus();
+    cy.url().should("include", urls.productSelectioCheckoutPage);
+  });
 });
--- cypress/pages/checkout.js
+++ cypress/pages/checkout.js
@@ -1,34 +1,40 @@
-import mainPage from "./product-selection";
+import productSelectionPage from "./product-selection";
 import { cartSelectors } from "./cart";
-export const checkoutSelectors = {
-returnToCartBtn: '["data-testid="edit-cart-btn"]',
-confirmOrderbtn: '["open-order-confirmation-btn"]'
-}
 import { textContent } from "../valid-data/info/validInfo";
-const productSelectionPage = new mainPage();
 
 class checkoutPage {
-    addDrugToTheCart(item) {
-        productSelectionPage.visit();
-        productSelectionPage.searchDrug();
-        productSelectionPage.deleteAllFromCartApi();
-        cy.contains(textContent.cartIsEmpty).should("exist");
-        productSelectionPage.addToCart(item)
-    };
+  addDrugToTheCart(item) {
+    const productSelection = new productSelectionPage();
+    productSelection.visit();
+    productSelection.searchDrug();
+    productSelection.deleteAllFromCartApi();
+    cy.contains(textContent.cartIsEmpty).should("exist");
+    productSelection.addToCart(item);
+  }
 
-    checkDrugStatus() {
-        cy.intercept("GET", `**/v1/cart/*/distributor-items*`).as("getDrugInfo")
-        cy.get(cartSelectors.viewCheckoutBtn).click()
-        cy.wait('@getDrugInfo').then((interception) => {
-            const response = interception.response.body;
-            expect(response).to.have.property('items').and.to.be.an('array');
-            expect(response.items.length).to.be.greaterThan(0);
-            expect(response.items[0].items[0]).to.have.property('quantity', 1);
-            expect(response.items[0].items[0]).to.have.property('payment_type', '100_0');
-            expect(response.items[0].items[0]).to.have.property('cart_offer');
-            expect(response.items[0].items[0].cart_offer).to.have.property('name', "auto_test_product");
-        })
-    }
+  checkDrugStatus() {
+    cy.intercept("GET", `**/v1/cart/*/distributor-items*`).as("getDrugInfo");
+    cy.get(cartSelectors.viewCheckoutBtn).click();
+    cy.wait("@getDrugInfo").then((interception) => {
+      const response = interception.response.body;
+      cy.wrap(interception.response?.statusCode).should("eq", 200);
+      expect(response).to.have.property("items").and.to.be.an("array");
+      expect(response.items.length).to.be.greaterThan(0);
+      expect(response.items[0].items[0]).to.have.property("quantity", 1);
+      expect(response.items[0].items[0]).to.have.property(
+        "payment_type",
+        "100_0",
+      );
+      expect(response.items[0].items[0]).to.have.property("cart_offer");
+      expect(response.items[0].items[0].cart_offer).to.have.property(
+        "name",
+        textContent.drugFullName,
+      );
+    });
+  }
 }
-
+export const checkoutSelectors = {
+  returnToCartBtn: '["data-testid="edit-cart-btn"]',
+  confirmOrderbtn: '["open-order-confirmation-btn"]',
+};
 export default checkoutPage;
