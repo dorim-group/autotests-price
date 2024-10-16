b01d9517deaf8d320c4c272f36c63a62bf9daa10
Maxim Voronin <maxim.voronin2@gmail.com>
2024-10-16 13:54:05 +0500

    Added tags, changed screen resolution

--- cypress/e2e/product-selection/PRICE-57.User moves into checkout.cy.js
+++ cypress/e2e/product-selection/PRICE-57.User moves into checkout.cy.js
@@ -3,7 +3,7 @@ import checkoutPage from "../../pages/checkout";
 import { textContent, urls } from "../../valid-data/info/validInfo";
 
 describe("PRICE-57.User moves to checkout page", () => {
-    it("PRICE-57.User moves to checkout page", () => {
+    it("PRICE-57.User moves to checkout page", { tags: ["dev","stage"] }, () => {
         cy.setAuthToken();
         const page = new checkoutPage
         page.addDrugToTheCart(0)
